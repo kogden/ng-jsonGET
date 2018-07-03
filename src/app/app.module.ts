ad904d26204775c70754a7ae340f61f08afc546e
Kevin Ogden <kogden2@albany.edu>
2018-07-03 09:24:25 -0400

    Animations and  RESTful calls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { UsersComponent } from './users/users.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,6 +26,7 @@ import { HttpModule } from '@angular/http';
     AppRoutingModule,
     HttpModule,
     HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
